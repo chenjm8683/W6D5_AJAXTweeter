6a5d73cf12476f3aae430591e27f1d39a617716e
Jingming Colin Chen <chenjm8683@gmail.com>
2015-12-04 17:09:38 -0800

    finished phase 3

---
+++ frontend/tweet_compose.js
@@ -0,0 +1,84 @@
+function TweetCompose(el) {
+  this.$el = $(el);
+  this.$content = this.$el.find('textarea');
+  this.$mention = this.$el.find('select')
+  this.$feed = $('#feed');
+
+  this.registerEvent();
+
+
+}
+
+
+
+TweetCompose.prototype.registerEvent = function () {
+  this.$el.on("submit", this.handleSubmit.bind(this));
+};
+
+TweetCompose.prototype.clearInput = function () {
+  // body...
+  this.$content.val("");
+  this.$mention.val(0);
+
+};
+
+
+TweetCompose.prototype.handleSubmit = function (e) {
+  e.preventDefault();
+  this.$el.find(":input").prop('disabled', true);
+
+  $.ajax({
+    url: "/tweets",
+    method: "POST",
+    data: {
+      "tweet[content]": this.$content.val(),
+      "tweet[mentioned_user_ids][]": this.$mention.val(),
+    },
+    dataType: "json",
+    success: function(message) {
+      console.log(message);
+      this.handleSuccess(message);
+    }.bind(this)
+  });
+};
+
+TweetCompose.prototype.handleSuccess = function (message) {
+  // body...
+    this.clearInput();
+    this.$el.find(":input").prop('disabled', false);
+    var $tweet = $('<li>');
+    $tweet.append(message.content + "--");
+    var $userLink = $('<a/>')
+      .attr('href', '/users/' + message.user_id)
+      .text(message.user.username);
+    $tweet.append($userLink)
+      .append("--" + message.created_at);
+
+    var $mentions = $('<ul>');
+    message.mentions.forEach( function(mention) {
+      var $mentionedUser = $('<li>');
+      var $mentionedUserLink = $('<a/>')
+        .attr('href', '/users/' + mention.user_id)
+        .text(mention.user.username);
+      $mentionedUser.append($mentionedUserLink);
+      $mentions.append($mentionedUser);
+    });
+
+    if ($mentions.children().length > 0) {
+      $tweet.append($mentions);
+    }
+
+    this.$feed.prepend($tweet);
+
+
+
+
+    // this.$feed.prepend()
+
+};
+
+
+
+
+
+module.exports = TweetCompose;
--- frontend/twitter.js
+++ frontend/twitter.js
@@ -1,5 +1,7 @@
 var FollowToggle = require('./follow_toggle');
 var UsersSearch = require('./users_search');
+var TweetCompose = require('./tweet_compose');
+
 
 $(function(){
   $('button.follow-toggle').each( function(_idx, el) {
@@ -9,4 +11,9 @@ $(function(){
   $('nav.users-search').each(function (_idx, el) {
     new UsersSearch(el);
   });
+
+  $('form.tweet-compose').each(function (_idx, el) {
+    new TweetCompose(el);
+  });
+
 });
